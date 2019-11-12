a172de872448232ae461d6f96a1a8d142ae76e91
yn1323 <yn1323@gmail.com>
2019-11-13 00:52:24 +0900

    herokuでの動作確認完了

--- run.py
+++ run.py
@@ -1,12 +1,24 @@
+from dotenv import load_dotenv
+from os.path import join, dirname
+import os
 from flask import Flask
-
+# パッケージ
+from scraping import pog
+# Flask
 app = Flask(__name__)
+# .env
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
+# URL = os.environ.get("URL") 
 
 @app.route("/")
 def hello_world():
-  return "Hello, World!\n"
+  a = pog.Pog(os.environ.get("URL"))
+  print(a.users)
+  return "Hello, Worlo!\n"
 
-
+# このif文が重要
 if __name__ == "__main__":
-    app.run()
+  # debug=Trueだと自動でリロードする
+  app.run(debug=True)
--- scraping/pog.py
+++ scraping/pog.py
@@ -1,15 +1,10 @@
-import scraping as scr
+from scraping.scraping import Scraping
 
 class Pog:
   def __init__(self, url):
-    self.scr = scr.Scraping(url)
+    self.scr = Scraping(url)
     self.users, self.prizes, self.recents, self.urls = \
       self.scr.getString('td.user'), \
       self.scr.getString('td.money'), \
       self.scr.getString('td.recent'), \
       self.scr.getUrl('td.user > a')
-    print(self)
-
-
-# a = Pog("http://pogstarion.com/groupuserlist.do?group_num=0526013548")
-# print(a.users)
